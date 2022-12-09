804d89304be4b480fdec4855d71ae4ed77135b2e
Hoyoung Son <xzxking17@gmail.com>
2022-12-09 21:31:33 +0900

    chore: :gear: @next/bundle-analyzer 설정
    - yarn build:analyze를 실행할 경우에만 동작

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
+const isAnalyze = process.env.ANALYZE === 'true';
+const withBundleAnalyzer = require('@next/bundle-analyzer')({enabled: isAnalyze});
+const analyzerConfig = isAnalyze
+  ? withBundleAnalyzer({
+      compress: true,
+      webpack(config) {
+        const prod = process.env.NODE_ENV === 'production';
+        return {
+          ...config,
+          mode: prod ? 'production' : 'development',
+          devtool: prod ? 'hidden-source-map' : 'eval',
+        };
+      },
+    })
+  : {};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...analyzerConfig,
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
