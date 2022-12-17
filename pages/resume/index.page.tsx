39990a1a0ba45f52a4044a2fb2fb0a1f1beca191
Hoyoung Son <xzxking17@gmail.com>
2022-12-17 15:44:59 +0900

    chore: :gear: docs 폴더 하위에 resumes 폴더 생성
    - 다양한 이력서를 작성할 수 있도록 폴더 생성
    - blog.data.ts에서 resumePage에 보여질 이력서를 선택할 수 있음
        - 보여질 이력서의 fileName 작성 (/docs/resumes에 있는 파일)

--- pages/resume/index.page.tsx
+++ pages/resume/index.page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {domainName, metadata, siteUrl} from '@root/blog.data';
+import {domainName, metadata, siteUrl, resumeFileName} from '@root/blog.data';
 import {getDocumentByFileName} from '@src/lib';
 import {ResumeTemplate} from '@src/resume';
 import {ResumeDocument, SeoHead} from '@src/shared';
@@ -28,7 +28,10 @@ export default ResumePage;
 
 // FUNCTIONS : NEXT.JS ===============================================
 export const getStaticProps = async () => {
-  const resumeDoc = await getDocumentByFileName<ResumeDocument>({fileName: 'resume.mdx'});
+  const resumeDoc = await getDocumentByFileName<ResumeDocument>({
+    subFolderType: 'resumes',
+    fileName: resumeFileName,
+  });
   return {
     props: {resumeDoc},
   };
