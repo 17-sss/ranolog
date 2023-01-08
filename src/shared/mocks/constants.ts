46a5e1b8575a9ab9b8950ccac597c80af662c5ce
Hoyoung Son <xzxking17@gmail.com>
2023-01-08 14:22:13 +0900

    docs: :books: README 수정
    - "글 목차 자동 생성 지원" 추가

--- src/shared/mocks/constants.ts
+++ src/shared/mocks/constants.ts
@@ -1,21 +1,21 @@
 import {PostDocument, ProjectDocument} from '../types';
 
 /* POST ==================================================== */
-export const sampleContentHtml = `<h3>> Headings</h3>
+export const sampleContentHtml = `<h1>> Headings</h1>
 <h1>h1</h1>
 <h2>h2</h2>
 <h3>h3</h3>
 <h4>h4</h4>
 <h5>h5</h5>
 <h6>h6</h6>
-<h3>> Texts &#x26; code &#x26; hr &#x26; anchor</h3>
+<h1>> Texts &#x26; code &#x26; hr &#x26; anchor</h1>
 <p><strong>bold</strong></p>
 <p><em>italic</em></p>
 <p><del>strikethrough</del></p>
 <p><code>code</code></p>
 <hr>
 <p><a href="about:blank">Anchor (about:blank)</a></p>
-<h3>> Contents</h3>
+<h1>> Contents</h1>
 <p>We recommend using <strong>Static Generation</strong> (with and without data) whenever possible because your page can be built once and served by CDN, which makes it much faster than having a <del>server</del> render the page <em>on</em> every request</p>
 <p>You can use <strong><em>Static Generation</em></strong> for many types of pages, including:</p>
 <ul>
@@ -38,7 +38,7 @@ export const sampleContentHtml = `<h3>> Headings</h3>
 <li>E-commerce product listings</li>
 <li>Help and documentation</li>
 </ul>
-<h3>> Table</h3>
+<h1>> Table</h1>
 <table>
 <thead>
 <tr>
@@ -70,7 +70,7 @@ export const sampleContentHtml = `<h3>> Headings</h3>
 </tr>
 </tbody>
 </table>
-<h3>> Blockquote</h3>
+<h1>> Blockquote</h1>
 <blockquote>
 <p>Blockquote??</p>
 <blockquote>
@@ -81,7 +81,7 @@ export const sampleContentHtml = `<h3>> Headings</h3>
 </blockquote>
 </blockquote>`;
 
-export const samplePrismHtml = `<h3>> Code Block</h3>
+export const samplePrismHtml = `<h1>> Code Block</h1>
 <div class="remark-highlight"><pre class="language-js"><code class="language-js"><span class="token keyword">const</span> num <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
 <span class="token keyword">const</span> str <span class="token operator">=</span> <span class="token string">'1'</span><span class="token punctuation">;</span>
 <span class="token keyword">function</span> <span class="token function">func</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
