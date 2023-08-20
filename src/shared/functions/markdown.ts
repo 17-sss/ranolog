3b4f6ee6680d198a1b1cae6392927f68e68a6e69
Hoyoung Son <xzxking17@gmail.com>
2023-08-20 22:45:31 +0900

    feat: :sparkles: shared/functions/markdown 수정 (next-mdx-remote 관련)
    - markdownToHtml 함수에서 사용하는 serialize 함수의 mdxOptions 수정
        - remarkPlugins에 remarkHtml 제거
        - development 프로퍼티 추가 (development: process.env.NODE_ENV === 'development')
            - 스토리북(개발모드)에서 발생하는 에러 (TypeError: _jsx is not a function) 해결을 위함
              참고: https://github.com/hashicorp/next-mdx-remote/issues/350

--- src/shared/functions/markdown.ts
+++ src/shared/functions/markdown.ts
@@ -1,14 +1,16 @@
-// @ts-ignore
 import mdxPrism from 'mdx-prism';
 import {serialize} from 'next-mdx-remote/serialize';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
-import remarkHtml from 'remark-html';
 
 /** [async] Markdown Text -> HTML 변환 (MDX 포함) */
 export const markdownToHtml = async (aMarkdown: string) => {
   const result = await serialize(aMarkdown, {
-    mdxOptions: {remarkPlugins: [remarkGfm, remarkHtml], rehypePlugins: [rehypeSlug, mdxPrism]},
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypeSlug, mdxPrism],
+      development: process.env.NODE_ENV === 'development',
+    },
   });
   return result;
 };
